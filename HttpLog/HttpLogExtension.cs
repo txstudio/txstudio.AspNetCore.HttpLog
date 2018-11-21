9d70040949946923913c6c34150c65bf7b880dfe
Shi Hao Wang <txstudio@outlook.com>
2018-11-21 11:59:07 +0800

    加入 HttpLog 的 ApplicationBuilder 擴充方法

--- HttpLog/HttpLogExtension.cs
+++ HttpLog/HttpLogExtension.cs
@@ -1,6 +1,17 @@
-﻿namespace HttpLogExtensions
-{
-    public class HttpLogExtension
-    { }
+﻿using Microsoft.AspNetCore.Builder;
 
+namespace HttpLogExtensions
+{
+    public static class HttpLogExtension
+    {
+        public static IApplicationBuilder UseHttpLog(this IApplicationBuilder builer)
+        {
+            return builer.UseMiddleware<HttpLogMiddleware>(null);
+        }
+
+        public static IApplicationBuilder UseHttpLog(this IApplicationBuilder builer, IHttpLogStore httpLogStore)
+        {
+            return builer.UseMiddleware<HttpLogMiddleware>(httpLogStore);
+        }
+    }
 }
